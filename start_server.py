c9c24b4e5a4b30ed06271f6571617abddf351cdd
jianhj <jianhongjun@umeweb.com>
2025-10-17 11:23:02 +0800

    cors

--- start_server.py
+++ start_server.py
@@ -3,6 +3,7 @@ import argparse
 import fire
 import uvicorn
 from loguru import logger
+from starlette.middleware.cors import CORSMiddleware
 
 from sorawm.configs import LOGS_PATH
 from sorawm.server.app import init_app
@@ -18,6 +19,13 @@ logger.add(LOGS_PATH / "log_file.log", rotation="1 week")
 def start_server(port=args.port, host=args.host):
     logger.info(f"Starting server at {host}:{port}")
     app = init_app()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # 允许所有域名
+        allow_credentials=True,
+        allow_methods=["*"],  # 允许所有方法
+        allow_headers=["*"],  # 允许所有头部
+    )
     config = uvicorn.Config(app, host=host, port=port, workers=args.workers)
     server = uvicorn.Server(config=config)
     try:
